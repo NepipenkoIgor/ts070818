df634022f05c0503d491a77c494534dc6b351a92
Alexey Ermolaev <ermolaev.mail@gmail.com>
2018-08-14 23:32:42 +0300

    add isInArray function

---
+++ lesson-2/ErmolaevAlexey/functions.ts
@@ -0,0 +1,36 @@
+// Обратите внимание на типизацию и подключите ts-lint (проверяем включен ли плагин в редакторе)
+// Все должно быть строго как на занятии, никаких any;
+// 1)   Написать функцию isInArray(), которая начиная со второго принимает переменное количество аргументов.
+//      Возвращает true, если все аргументы, кроме первого входят в первый.
+//      Первым всегда должен быть массив.
+
+function isInArray<T>(sourceArray: T[], ...targetElements: T[]): boolean {
+    if (targetElements.length === 0) {
+        console.error('isInArray() function warning: targetElements is undefined');
+        return false;
+    }
+    return targetElements.every((value: T) => {
+        return sourceArray.indexOf(value) >= 0;
+    });
+}
+
+let mySourceArray: number[] = [1, 2, 3, 4, 10];
+
+console.log('1. isInArray(): TRUE ::', isInArray(mySourceArray, 1, 2, 10, 3));
+console.log('2. isInArray(): FALSE ::', isInArray(mySourceArray, 1, 2, 20));
+console.log('3. isInArray(): FALSE ::', isInArray(mySourceArray, 100));
+console.log('4. isInArray(): FALSE + warning ::', isInArray(mySourceArray));
+
+// 2)   Написать функцию summator(), которая сумирует переданые ей аргументы.
+//      Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено. Проверяйте NaN
+
+// 3)   Написать функцию getUnique(arr), которая принимает аргументом неограниченое число аргументов,
+//      и возвращает массив уникальных элементов. Аргумент не должен изменяться.
+//      Порядок элементов результирующего массива должен совпадать с порядком,
+//      в котором они встречаются в оригинальной структуре.
+
+// 4)   Написать функцию котороя будет разворачивать буквы в словах предложения, но только лишь буквы
+//      цифры и специальные символы должны остаться на месте
+//      s1tar3t 2 hellow  ->  t1rat3s 2 wolleh
+//      s1ta$%r3t 2 hel^low  ->  t1ra$%t3s 2 wol^leh
+//      s1tar3t 2   low5  ->  t1rat3s 2   wol5
