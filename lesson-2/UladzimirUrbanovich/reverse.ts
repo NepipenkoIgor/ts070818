e7136321cf28a652642a6512879ea8cb1469f000
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2018-08-15 12:38:25 +0300

    Refactor revers function

--- lesson-2/UladzimirUrbanovich/reverse.ts
+++ lesson-2/UladzimirUrbanovich/reverse.ts
@@ -1,8 +1,26 @@
 function reverseSentence(sentence: string): string {
-    return sentence
-        .split(/\b/)
+    const splittedSentence: string[] = splitSentence(sentence);
+    return splittedSentence
         .map((word: string) => reverseWord(word))
         .join('');
+}
+
+function splitSentence(sentence: string): string[] {
+    const regExp: RegExp = /[\s,.]+/g;
+    let match: RegExpExecArray | null = regExp.exec(sentence);
+    const result: string[] = [];
+    let lastIndex: number = 0;
+
+    while (match !== null) {
+        const word: string = sentence.slice(lastIndex, match.index);
+        const splitterLength: number = match[0].length;
+
+        lastIndex = match.index + splitterLength;
+        result.push(word, match[0]);
+        match = regExp.exec(sentence);
+    }
+
+    return result;
 }
 
 function reverseWord(word: string): string {
