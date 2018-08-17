f2cd8a6683e043c56ff6d46e14f998ae96060d25
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2018-08-17 12:53:33 +0300

    Add comments

--- lesson-2/UladzimirUrbanovich/reverse.ts
+++ lesson-2/UladzimirUrbanovich/reverse.ts
@@ -1,4 +1,7 @@
 export function reverseSentence(sentence: string): string {
+    // Split sentence using /\s/, '.' or ',' signs.
+    // ('hell\noeeew  ,jii.wefff  ' ==> ['hell', '\n', 'oeeew', '  ,', 'jii', '.', 'wefff'])
+    // If this functionality is excess you can use sentence.split(' ').map(...).join(' ');
     const splittedSentence: string[] = splitSentence(sentence);
     return splittedSentence
         .map((word: string) => reverseWord(word))
@@ -34,10 +37,12 @@ function reverseWord(word: string): string {
     }
 
     const letters: string[] = word.split('');
+    // extract special signs and revers
     const reversed: string[] = letters
         .filter((letter: string) => wordRegExp.test(letter))
         .reverse();
 
+    // paste special signs in their indexes
     letters.forEach((letter: string, index: number) => {
         if (wordRegExp.test(letter)) { return; }
         reversed.splice(index, 0, letter);
