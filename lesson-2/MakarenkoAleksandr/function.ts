edee87afac548745cfb3ce98d53c74e5319a666f
MakarenkoAlexandr <38201224+MakarenkoAlexandr@users.noreply.github.com>
2018-08-20 22:59:40 +0600

    Update function.ts

--- lesson-2/MakarenkoAleksandr/function.ts
+++ lesson-2/MakarenkoAleksandr/function.ts
@@ -1,34 +1,21 @@
-import { Component } from '@angular/core';
-
-function isInArray(array:string[], ...args:string[]) : boolean
-{
-    let result : boolean;
-    args.forEach(elementArg => {
-        result = array.includes(elementArg);
-    });   
+function isInArray<T>(array: T[], ...args: T[]): boolean {
+    return args.every(element => array.includes(element));
+  }
+  console.log('isInArray ' + isInArray(Array('Mary', 'Tom', 'Jack', 'Jill2'), 'Mary', 'Tom', 'Jack2'));
+  
+  function summator(...args: (string | number)[]) {
+    const result = args.reduce((accumulator: number, element: number) => {
+      if (typeof element === 'number' && !isNaN(element)) {
+        return accumulator += element;
+      } else if (typeof element === 'string' && !isNaN(Number(element))) {
+        return accumulator += Number(element);
+      }
+    }, 0);
     return result;
-}
-
-var names:string[] = new Array("Mary","Tom","Jack","Jill");
-console.log("isInArray " + isInArray(names, "Mary", "Tom", "Jack2"));
-
-function summator(...args:(string|number)[]) : number
-{
-  let result : number = 0;
-  args.forEach(element =>{
-    if(typeof element === "number" && !isNaN(element))
-      result += element
-    else if(typeof element === "string")
-      if(!isNaN(Number.parseInt(element)))
-        result += Number.parseInt(element)
-  })
-  return result;
-}
-console.log("Summ "  + summator(1,2,'3', 4, 5).toString());
-
-function getUniq(...args:(number)[]) : number[]
-{
-  return Array.from(new Set(args));
-}
-
-console.log(getUniq(8,1,1,2,3,5,6,6,7));
+  }
+  console.log('Summ ' + summator(1, 2, '3', 4, 5).toString());
+  
+  function getUnique(...args: number[]): number[] {
+    return Array.from(new Set(args));
+  }
+  console.log(getUnique(8, 1, 1, 2, 3, 5, 6, 6, 7));
